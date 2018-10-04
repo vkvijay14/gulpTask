b0ffdf9e303a1706e75c86552782535852401577
vijay kumar <vkvijaykumar14@gmail.com>
2018-10-04 19:20:15 +0530

    file remove gulpfile.js

--- src/js/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-gulp.task('script', function(){
-	return gulp.src('src/js/*.js')
-		.pipe(concat('newFile.js'))
-		.pipe(gulp.dest('src/js/'))
-});
-gulp.task('default', ['script']);
